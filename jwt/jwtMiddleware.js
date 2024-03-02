f2c62785846edd9c1892522f5f21295963a5d54d
kacem <kacemmeherzi52@gmail.com>
2024-03-02 18:25:37 +0100

    token verfication function improved

--- jwt/jwtMiddleware.js
+++ jwt/jwtMiddleware.js
@@ -1,15 +1,25 @@
 
+const jwt = require("./jwtUtils");
 
-
-function extractToken(req, res, next) {
+function extract_validate_Token(req, res, next) {
     const authorizationHeader = req.headers.authorization;
   
     if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
       const token = authorizationHeader.substring(7); // Remove 'Bearer ' prefix
       req.token = token;
+      console.log(jwt.verifytoken(token))
+      const validity = jwt.verifytoken(token);
+      if (validity.valid){
+         next();
+
+      }else{res.status(403).json({"messsage":validity.message})}
+
+    }else {
+        
+         res.status(403).json({messsage:'unauthorized ye bro hot token fel header'})
     }
   
-    next();
+    
   }
   
-  module.exports = extractToken;
+  module.exports = extract_validate_Token;
