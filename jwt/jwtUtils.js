dd972375c795b880539f7f5387b37dd8083615d4
kacem <kacemmeherzi52@gmail.com>
2024-03-08 01:34:48 +0100

    new reservation token validation added // improved

--- jwt/jwtUtils.js
+++ jwt/jwtUtils.js
@@ -6,10 +6,10 @@ function generateToken(user) {
   const secretKey = process.env.SECRET_KEY;
   const payload = {
     iss: "kacem",
-    sub: user.username,
+    
     iat: Math.floor(Date.now() / 1000),
     exp: Math.floor(Date.now() / 1000) + 3600 * 2, // 2 heure
-    customClaim: "some_value",
+    customClaim: user._id,
     roles: ["admin", "user"],
   };
   return jwt.sign(payload, secretKey);
@@ -18,17 +18,30 @@ function generateToken(user) {
 function verifytoken(token) {
   var valid = true;
   var message;
+  var payload ;
   const secretKey = process.env.SECRET_KEY;
   jwt.verify(token, secretKey, (err, decoded) => {
     //console.log("aaaaaaaaa",decoded)
     if (err) {
       message = err.message;
       valid = false;
-    }
+    }else {payload = decoded}
   });
 
-  return { valid: valid, message: message };
+  return { valid: valid, message: message , payload :payload };
 }
 //TODO :: refrech token methood
 
-module.exports = { generateToken, verifytoken };
+
+function generateToken_comfirm(reservation) {
+  const secretKey = process.env.SECRET_KEY;
+  const payload = {
+    iss: "kacem",
+    iat: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + 3600 * 2, // 2 heure
+    resid: reservation._id,
+    roles: ["admin", "user"],
+  };
+  return jwt.sign(payload, secretKey);
+}
+module.exports = { generateToken, verifytoken,generateToken_comfirm };
