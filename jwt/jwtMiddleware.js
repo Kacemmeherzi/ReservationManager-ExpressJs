1724f6cc1c48c6bbb5ae212ac33d563b323b1da5
kacem <kacemmeherzi52@gmail.com>
2024-03-16 04:21:59 +0100

    token payload added to the request from the first extraction

--- jwt/jwtMiddleware.js
+++ jwt/jwtMiddleware.js
@@ -4,11 +4,13 @@ function extract_validate_Token(req, res, next) {
   const authorizationHeader = req.headers.authorization;
 
   if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
-    const token = authorizationHeader.substring(7); // Remove 'Bearer ' prefix
+    const token = authorizationHeader.substring(7); // Remove 'Bearer ' prefix //OR substring('')[1]
     req.token = token;
     console.log(jwt.verifytoken(token));
     const validity = jwt.verifytoken(token);
     if (validity.valid) {
+      req.tokenpayload = validity.payload
+      console.log(validity.payload);
       next();
     } else {
       res.status(403).json({ messsage: validity.message });
