17d0072fdabccf1a4aaa2f48b72759e4b4ea9532
kacem <kacemmeherzi52@gmail.com>
2024-03-19 05:04:50 +0100

    authRoutes added

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const authController = require('../controllers/authController.js')
+
+
+router.get('/login',authController.login)
+router.post('/register',authController.register)
+
+
+module.exports = router;
