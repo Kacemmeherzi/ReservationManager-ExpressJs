54590795b849aa60e1edd9c8c271bb38206ea4b2
kacem <kacemmeherzi52@gmail.com>
2024-03-19 05:13:26 +0100

    fixeed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController.js')
 
 
-router.get('/login',authController.login)
+router.post('/login',authController.login)
 router.post('/register',authController.register)
 
 
