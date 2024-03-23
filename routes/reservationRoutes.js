d7136240d3ed13ec608a2a01896f762f17f75071
kacem <kacemmeherzi52@gmail.com>
2024-03-23 05:27:22 +0100

    fixed

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -8,7 +8,8 @@ const res_verification = require("../services/reservation_verifcation.js");
 
 
 router.get('/',reservationController.getallreservations)
-router.get('/:id',reservationController.getreservationbyid)
+router.get('/reservationbyid/:id',reservationController.getreservationbyid)
+router.get('/byuserid/:id',reservationController.reservationbyuserid)
 router.post('/add',res_verification.verif_user,res_verification.verif_room,reservationController.addreservation)
 router.delete('/delete/:id',reservationController.deletereservationbyid)
 router.put('/update/:id', reservationController.updateReservation)
