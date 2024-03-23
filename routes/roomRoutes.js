7b6c1e245aec04a8f9b15d8e72042a060f1df8a3
kacem <kacemmeherzi52@gmail.com>
2024-03-23 05:27:09 +0100

    fixed

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router()
 const roomController = require('../controllers/roomcontroller')
 
 router.get('/',roomController.getallrooms)
-router.get('/:id',roomController.getroombyid)
+router.get('/roombyid/:id',roomController.getroombyid)
 router.put('/update',roomController.updateroombyid)
 router.delete('/delete',roomController.deleteroombyid)
 router.post('/add',roomController.addroom)
 
-
+router.get('/freerooms',roomController.getunreservedrooms)
 
 module.exports = router 
