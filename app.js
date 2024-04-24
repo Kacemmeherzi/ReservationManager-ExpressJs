fac9b8ed233bd9693ed31681925e7ce0bb1d99a1
kacem <kacemmeherzi52@gmail.com>
2024-04-24 16:36:06 +0200

    improved

--- app.js
+++ app.js
@@ -8,18 +8,30 @@ const userRoutes = require('./routes/userRoutes.js')
 const jwt = require("./jwt/jwtUtils.js");
 const authRoutes = require("./routes/authRoutes.js");
 const roomRoutes = require("./routes/roomRoutes.js");
+const operationRoutes = require("./routes/operationRoutes.js");
+const reservationController = require("./controllers/reservationcontroller.js")
+
 dotenv.config();
 const cors = require("cors");
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT || 3000;
 const mailservice = require("./notificationmanager/mailservice.js");
 const reservationRoutes = require("./routes/reservationRoutes.js");
-
+//CORS SITTINGS
+const corsOptions = {
+  origin: "http://localhost:4200/", // the client side dev origin
+  //optionsSuccessStatus: 200,
+};
+//app.use(cors(corsOptions));
+app.use(cors());
 //ROUTES
 app.use("/auth", cors(), authRoutes);
-app.use("/user", jwtMiddleware, jwt.has_admin_role, userRoutes);
-app.use("/room", jwtMiddleware, jwt.has_admin_role, roomRoutes);
+app.use("/user", cors(),jwtMiddleware, jwt.has_admin_role, userRoutes);
+app.use("/room",cors(), jwtMiddleware, jwt.has_admin_role, roomRoutes);
 app.use("/reservation", jwtMiddleware,jwt.has_admin_role,reservationRoutes);
+app.use("/operation", operationRoutes);
+app.use("/confirm", reservationController.confirmToken);
+
 
 //SWAGGER
 const swaggerUi = require("swagger-ui-express");
@@ -32,13 +44,7 @@ app.post("/testmail", async (req, res) => {
   await mailservice.sendMail(email).then(res.send("done"));
 });
 
-//CORS SITTINGS
-const corsOptions = {
-  origin: "http://localhost:4200/", // the client side dev origin
-  //optionsSuccessStatus: 200,
-};
-//app.use(cors(corsOptions));
-app.use(cors());
+
 //Mongo connct
 mongoose
   .connect(MONGO_URI)
