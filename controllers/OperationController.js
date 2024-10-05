944b87c85201a2341f83383dd9117fa4f43e603f
kacem <kacemmeherzi52@gmail.com>
2024-10-05 22:04:18 +0100

    fixed

--- controllers/OperationController.js
+++ controllers/OperationController.js
@@ -2,23 +2,25 @@ const jwt = require("../jwt/jwtUtils.js");
 const Operation = require("../models/operation.js")
 const mailservice = require("../notificationmanager/mailservice.js");
 const User = require ("../models/user.js")
+const Reservation = require("../models/reservation.js");
+
 const OperationController = {
 
 createOperation : async(req , res) => {
     try {
 const userid = req.body.userid       
-const  oper  = req.body.oper ;
+const  opera  = req.body.oper ;
 const  resid  = req.body.resid
 const token = jwt.generateToken_comfirm(resid)
-const content = req.body.content
+const contents = req.body.content
 const user = User.findById(userid)
 const operation = await new Operation({
-    op : oper ,
+    action : opera ,
     reservationid : resid,
     token : token,
-    content : content
+    content : contents
 }).save()
-const mailOptions = mailservice.createmailbody(user, token,);
+const mailOptions = mailservice.createmailbody(user, token);
 //send the mail
 await mailservice.sendMail(mailOptions);
 res
@@ -50,13 +52,10 @@ res.status(400).json({ message: e.message });
 ,
  confirmToken :  async (req, res) => {
         const token = req.params.token;
-        const data = jwt.verifytoken(token);
-        if (data.valid) {
-          console.log(data.payload.resid);
-          const reservation = await Reservation.findByIdAndUpdate(
-            data.payload.resid,
-            { validation: true },
-          );
+        const operation = Operation.findOne({"token": token})
+        
+        if (operation.op=='delete') {
+          const reservation = await Reservation.findByIdAndDelete(opertion.Reservationid)
           res.send("<h1> COMFIRMED </h1>");
         } else {
           res.send("<h> ERROR </h1>");
